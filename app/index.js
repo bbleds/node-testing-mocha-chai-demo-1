1990c90cd8b3453a937b502825ab89e06e5f6913
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-02 10:42:07 -0600

    adding tests

--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
 "use strict";
 
-// good for small projects
-require("babel-register");
-require("./app");
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  return res.status(200).send("ok");
+});
+
+const server = app.listen(app.get("port"), (err) => {
+  if (err) throw err;
+  console.log(`listening on port ${server.address()}`);
+});
+
+
+module.exports = app;
---
+++ test/demo.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("math", () => {
+  it("should add 1 and 1", () => {
+      expect()
+  });
+});
